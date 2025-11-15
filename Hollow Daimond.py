f380a25aa56af6cc16f3dc63f347302a63c16567
Kishan Gupta <kishanbca@outlook.com>
2025-11-15 21:47:44 +0530

    Add hollow diamond pattern script
    Introduces a Python script to print a hollow diamond pattern for a given size n. The script uses loops and string multiplication to generate the pattern.

---
+++ Hollow Daimond.py
@@ -0,0 +1,13 @@
+# Hollw daimond
+n=8
+for i in range(1,n+1):
+    if i==1:
+        print(" "*(n-i)+"*")
+    else:
+        print(" "*(n-i)+"*"+ " "*(2*i-3)+"*")
+for i in range(n-1,0,-1):
+    if i==1:
+        print(" "*(n-i)+"*")
+    else:
+        print(" "*(n-i)+"*"+ " "*(2*i-3)+"*")
+    
