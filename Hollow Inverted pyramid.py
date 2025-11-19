6aa255014b4132d0ed24454a40271402550899b3
Kishan Gupta <kishanbca@outlook.com>
2025-11-19 16:40:04 +0530

    Update Hollow Inverted pyramid.py
    "Update Hollow inverted Pyramid.py with Hollow pattern logic"

--- Hollow Inverted pyramid.py
+++ Hollow Inverted pyramid.py
@@ -1,4 +1,10 @@
 # Hollow inverted pyramid
-n = 8
+n = 6
 for i in range(n, 0, -1):
-    print(' ' * (n - i) + '* ' * i)
+    if i==n:
+        print("* " * (2 * n - 1))
+    elif i==1:
+        print(" " * (n - 1) + "*")
+    else:
+        inner=" " * (2 * i - 3)
+        print(" " * (n - i) + "*" + inner + "*")
